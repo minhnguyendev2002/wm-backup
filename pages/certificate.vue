18ea0065fad3eb888a946068fdd3f2a18ebaa829
minhnguyendev2002 <85899672+minhnguyendev2002@users.noreply.github.com>
2023-02-17 18:19:33 +0700

    [FIX] feedback

--- pages/certificate.vue
+++ pages/certificate.vue
@@ -8,7 +8,7 @@
             <div class="container mx-auto text-center lg:text-right min-h-[300px] md:min-h-[400px] xl:min-h-[550px] flex flex-col">
                 <div class="overflow-y-hidden flex flex-col justify-center items-center mb-auto flex-1">
                     <h1 class="text-center text-5xl lg:text-[80px] font-bold lg:leading-[90px] text-white relative z-[1] animate-[fade-top_1s_linear_.5s_forwards] opacity-0 translate-y-[100%]">
-                        Các giải thưởng của đội ngũ QLTC
+                        Các giải thưởng của Đội ngũ QLTC
                     </h1>
                     <h2 class="xl:max-w-[80%] text-center mx-auto relative z-[1] text-2xl sm:text-4xl sm:text-[30px] font-medium mb-11 text-white animate-[fade-top_.7s_linear_1s_forwards] opacity-0 translate-y-[100%]">
                         Khi bạn lựa chọn Đội ngũ QLTC Wealth Management là bạn đã lựa chọn đúng một đối tác lâu dài và đáng tin cậy.
