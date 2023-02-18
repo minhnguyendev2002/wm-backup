82cf0b7b88f12ecc3cbd2fadc84661f011e68886
minhnguyendev2002 <85899672+minhnguyendev2002@users.noreply.github.com>
2023-02-18 15:40:27 +0700

    [FEAT] add buttoin

--- components/shared/ScrollToTop.vue
+++ components/shared/ScrollToTop.vue
@@ -1,8 +1,8 @@
 <template>
     <button
         aria-label="scroll top"
-        :class="scroll ? 'sm:top-[calc(100vh-10%)] top-[calc(100vh-20%)]' : '-top-[150px]'"
-        class="fixed z-50 w-14 h-14 rounded-full bg-prim-100 text-white hover:bg-prim-100 transition-all duration-700 right-8"
+        :class="scroll ? 'sm:top-[calc(100vh-10%)] top-[calc(100vh-22%)]' : '-top-[150px]'"
+        class="fixed z-50 w-14 h-14 rounded-full bg-prim-100 text-white hover:bg-prim-100 transition-all duration-700 right-4 sm:right-8"
         @click="scrollToTop"
     >
         <span><i class="fas fa-arrow-up" /></span>
